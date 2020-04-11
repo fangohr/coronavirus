140a7ff76d9806df7b7e335ddd1be03cb82faf25
Hans Fangohr <fangohr@soton.ac.uk>
2020-04-11 09:32:29 +0200

    Towards new directory structures

--- cachedir/joblib/coronavirus/fetch_deaths_last_execution/func_code.py
+++
@@ -1,5 +0,0 @@
-# first line: 51
-@joblib_memory.cache
-def fetch_deaths_last_execution():
-    """Use to remember at what time and date the last set of deaths was downloaded"""
-    return datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
